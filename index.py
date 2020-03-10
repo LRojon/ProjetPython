c0a6c4143a3112f8d9e54e4c484926db2dec5c56
LRojon <loic.rojon@gmail.com>
2020-03-10 11:27:57 +0100

    Modif hello -> index

---
+++ index.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	return '<h1>Hello World !!</h1><br><a href="/john"><button>john</button></a>'
+    
+@app.route('/john')
+def vntm():
+	return '<h3> vntm :) </h3>'
+
+	
+
